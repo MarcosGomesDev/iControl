769b5f575b812288244b70f10f197ab7cf55a1ee
Marcos Gomes <marcosgomesdev@gmail.com>
2023-10-16 23:39:14 -0300

    feat: create plus button component

---
+++ src/components/PlusButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Text from '../Text';
+
+import { PlusButtonContainer } from './styles';
+
+export function PlusButton() {
+  return (
+    <PlusButtonContainer>
+      <Text>Adicionar</Text>
+    </PlusButtonContainer>
+  );
+}
